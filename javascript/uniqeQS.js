e7b27ba2223ca6f3358242430321fae52931a4a0
shubhamiscodding <shubham.modi.cg@gmail.com>
2025-12-11 14:20:44 +0530

    nothing just learninig

---
+++ javascript/uniqeQS.js
@@ -0,0 +1,30 @@
+// function handleValues(a, b) {
+
+//   if (typeof(a) == typeof(b) && (!isNaN(Number(a)) && !isNaN(Number(b)))) {
+//     console.log(a + b);
+//     return;
+//   }
+
+//   if ((typeof(a) != typeof(b)) && (!isNaN(Number(a)) && !isNaN(Number(b)))) {
+//     a = a * 1;
+//     b = b * 1;
+//     console.log(a + b);
+//     return;
+//   }
+
+//   if (isNaN(Number(a)) || isNaN(Number(b))) {
+//     console.log(a + b);
+//     return;
+//   }
+
+// }
+
+
+// handleValues(5, 5);         
+// handleValues(5, "15");      
+// handleValues(5, "6b");    
+// handleValues("7", "8");    
+// handleValues("11.5", 15);   
+// handleValues(10, 20);
+
+
