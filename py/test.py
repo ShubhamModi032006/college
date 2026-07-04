5297832a4a0d5e0d2f20324a8d640805b73630c5
shubhamiscodding <shubham.modi.cg@gmail.com>
2026-07-04 09:48:38 +0530

    learning  python from start

---
+++ py/test.py
@@ -0,0 +1,84 @@
+# # name = "shubham"
+# # age = 20
+# # num = 2.0
+
+# # print("type of name :", type(name))
+# # print("type of age :", type(age))
+# # print("type of num :", type(num))
+
+
+
+# # num1 = int(input("Enter first number: "))
+# # num2 = int(input("Enter second number: "))
+
+# # print("Sum:", num1 + num2)
+# # print("Difference:", num1 - num2)
+# # print("Product:", num1 * num2)
+
+# # if num2 != 0:
+# #     print("Devide:", num1 / num2)
+# # else:
+# #     print("Quotient: Cannot divide by zero")
+
+
+# x = "5"
+# y = 3
+# print(x * y)
+
+
+
+# a = "where are you from?"
+
+# b = "what's your name?"
+
+# print(type(a))
+# print(b)
+
+
+# a = "Python is Larg Language"
+
+# print(a[8::-1])
+
+# print(a.lower())
+# print(a.upper())
+# print(a.title())
+# print(a.capitalize())
+# print(a.swapcase())
+
+
+
+a = "----helllo world-----------"
+
+# print(a.strip("-"))
+# print(a.lstrip("-"))
+# print(a.rstrip("-"))
+
+
+# # a = "hello"
+
+# # b = a
+
+
+# print(id(a))
+# print(id(b))
+
+# print(a.find("--"))
+# print(a.index("xy"))
+# print(a.count("-"))
+
+
+a = [10, 20, 30, 40]
+ 
+
+print(a[-2:])
+print(a[-100:])
+print(a[5:])
+print(a[0:-2])
+print(a[::-1])
+print(a[4::-1])
+print(a[99:0:-1])
+print(a[-2::-1])
+print(a[4:-100:-1])
+print(a[::-1])
+print(a[4:-2:-1])
+print(a[3:99:-1])
